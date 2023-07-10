66da95c64400b32b1bf3f7d824dc0e738de96cb1
Christoph Barbian <christoph.barbian@sap.com>
2023-07-10 12:09:25 +0200

    initial commit

---
+++ api/v1alpha1/types.go
@@ -0,0 +1,91 @@
+/*
+SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and reactivejob-operator-cop contributors
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/sap/component-operator-runtime/pkg/component"
+	componentoperatorruntimetypes "github.com/sap/component-operator-runtime/pkg/types"
+)
+
+// ReactiveJobOperatorSpec defines the desired state of ReactiveJobOperator.
+type ReactiveJobOperatorSpec struct {
+	component.Spec `json:",inline"`
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:default=1
+	ReplicaCount int `json:"replicaCount,omitempty"`
+	// +optional
+	Image                          component.ImageSpec `json:"image"`
+	component.KubernetesProperties `json:",inline"`
+}
+
+// ReactiveJobOperatorStatus defines the observed state of ReactiveJobOperator.
+type ReactiveJobOperatorStatus struct {
+	component.Status `json:",inline"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:printcolumn:name="State",type=string,JSONPath=`.status.state`
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+// +genclient
+
+// ReactiveJobOperator is the Schema for the reactivejoboperators API.
+type ReactiveJobOperator struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec ReactiveJobOperatorSpec `json:"spec,omitempty"`
+	// +kubebuilder:default={"observedGeneration":-1}
+	Status ReactiveJobOperatorStatus `json:"status,omitempty"`
+}
+
+var _ component.Component = &ReactiveJobOperator{}
+
+// +kubebuilder:object:root=true
+
+// ReactiveJobOperatorList contains a list of ReactiveJobOperator.
+type ReactiveJobOperatorList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ReactiveJobOperator `json:"items"`
+}
+
+func (s *ReactiveJobOperatorSpec) ToUnstructured() map[string]any {
+	result, err := runtime.DefaultUnstructuredConverter.ToUnstructured(s)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func (c *ReactiveJobOperator) GetDeploymentNamespace() string {
+	if c.Spec.Namespace != "" {
+		return c.Spec.Namespace
+	}
+	return c.Namespace
+}
+
+func (c *ReactiveJobOperator) GetDeploymentName() string {
+	if c.Spec.Name != "" {
+		return c.Spec.Name
+	}
+	return c.Name
+}
+
+func (c *ReactiveJobOperator) GetSpec() componentoperatorruntimetypes.Unstructurable {
+	return &c.Spec
+}
+
+func (c *ReactiveJobOperator) GetStatus() *component.Status {
+	return &c.Status.Status
+}
+
+func init() {
+	SchemeBuilder.Register(&ReactiveJobOperator{}, &ReactiveJobOperatorList{})
+}
